23a402f8a647c0b2d0ef21e279b768d929d69ec6
Tanjim Mollik <tanzim0627@icloud.com>
2022-12-12 00:04:50 +0600

    updated

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ import re
 password = input("Enter a password: ")
 pattern = re.compile(r"\d[a-zA-Z0-9@#$%]{6,}")
 
-b = pattern.fullmatch(password)
+check = pattern.fullmatch(password)
 
 try:
-    if b.group() != None:
+    if check.group() is not None:
         print("Password is valid.")
 except AttributeError:
     print("Password does not meet criteria.")
