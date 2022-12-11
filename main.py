552d3c765c888f596a49a147793143c8e665dbaf
Tanjim Mollik <tanzim0627@icloud.com>
2022-12-12 00:03:12 +0600

    done

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Regex Excercise")
+# Password Checker
+# Minimum 6 char,
+# Start with number,
+# can use: letters, numbers, @#$%
+import re
+password = input("Enter a password: ")
+pattern = re.compile(r"\d[a-zA-Z0-9@#$%]{6,}")
+
+b = pattern.fullmatch(password)
+
+try:
+    if b.group() != None:
+        print("Password is valid.")
+except AttributeError:
+    print("Password does not meet criteria.")
